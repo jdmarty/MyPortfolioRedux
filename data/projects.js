28e3a24320788410e41dcc74ab075f0245b86ba3
JD Marty <joshua.dmarty@gmail.com>
2021-01-03 16:40:32 -0800

    resized images and added fade-rise class

---
+++ data/projects.js
@@ -0,0 +1,55 @@
+const codeQuiz = {
+  title: "Code Quiz",
+  image: {
+    src: "assets/Images/codeQuiz.PNG",
+    alt: "Code Quiz",
+  },
+  description:
+    "Quiz app that prompts the user to answer web-development related questions and maintains a high score board.",
+  tech: ["HTML", "CSS", "BootStrap", "jQuery"],
+  repoLink: "https://github.com/jdmarty/codeQuiz",
+  deployedLink: "https://jdmarty.github.io/codeQuiz/",
+};
+
+const nutriSpot = {
+  title: "NutriSpot",
+  image: {
+    src: "assets/Images/nutriSpot.PNG",
+    alt: "Nutrispot",
+  },
+  description:
+    "Application that uses the Spoonacular and Yelp API to combine a healthy recipe search and a search for local restaurants",
+  tech: ["Bulma CSS", "jQuery", "Spoonacular API", "Yelp API"],
+  repoLink: "https://github.com/jdmarty/scoop",
+  deployedLink: "https://jdmarty.github.io/scoop/",
+};
+
+const passwordGenerator = {
+  title: "Password Generator",
+  image: {
+    src: "assets/Images/PasswordGenerator.PNG",
+    alt: "Password Generator",
+  },
+  description:
+    "Simple site that prompts you to enter password criteria and generates a scrambled password that meets those criteria",
+  tech: ["HTML", "CSS", "JavaScript"],
+  repoLink: "https://github.com/jdmarty/scrambledPasswordGenerator",
+  deployedLink: "https://jdmarty.github.io/scrambledPasswordGenerator/",
+};
+
+const jammming = {
+  title: "Jammming with JD",
+  image: {
+    src: "./assets/Images/jammming_codecademy.PNG",
+    alt: "Jamming with JD",
+  },
+  description:
+    "An app built with React and the Spotify API that allows users to search the Spotify library and create new playlists.",
+  tech: ["React", "Spotify API", "JavaScript"],
+  repoLink: "https://github.com/jdmarty/jammming/",
+  deployedLink: "http://jammmingwithjd.surge.sh/",
+};
+
+const Projects = [nutriSpot, codeQuiz, jammming, passwordGenerator]
+
+export default Projects
---
+++ scripts/portfolio.js
@@ -0,0 +1,48 @@
+import Projects from '../data/projects.js'
+
+function renderHighlight(project) {
+    //image, title, and description
+    $('#featured-image').attr('src', project.image.src).attr('alt', project.image.alt);
+    $('#featured-title').text(project.title);
+    $('#featured-description').text(project.description);
+    //technology
+    $("#featured-tech-cont").empty();
+    project.tech.forEach(tech => {
+        const techItem = $('<span>').addClass('featured-tech bg-primary').text(tech)
+        $("#featured-tech-cont").append(techItem);
+    });
+    //links
+    $("#featured-deploy-link").attr("href", project.repoLink);
+    $("#featured-repo-link").attr("href", project.deployedLink);
+}
+
+function renderCards(projects) {
+    let delay = 0;
+    projects.forEach((project, index) => {
+        //create elements
+        const card = $("<article>")
+            .addClass('col-12 col-lg-3 project-card fade-rise')
+            .attr("style", "width: 18rem;")
+            .attr('data-index', index);
+        const cardImg = $("<img>")
+          .addClass("project-card-img grow")
+          .attr("src", project.image.src)
+          .attr("alt", project.image.alt)
+        //create card with event listener
+        card
+            .append(cardImg)
+            .on('click', (e) => {
+                const index = $(e.currentTarget).attr('data-index')
+                renderHighlight(Projects[index])
+            })
+        //append card and column
+        setTimeout(() => {
+            $("#projects-card-row").append(card);
+        }, delay)
+        delay += 250
+    })
+}
+
+renderCards(Projects)
+
+renderHighlight(Projects[0])
